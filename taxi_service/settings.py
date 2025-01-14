d9bd1687a1e35e36cfefe07ae746f98247c91909
SashaHalytska <134492769+SashaHalytska@users.noreply.github.com>
2025-01-14 19:31:02 +0200

    Setting postgress configuration to docker-compose.yml and django settings

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -93,9 +93,13 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'taxi_db'),
+        'USER': os.getenv('POSTGRES_USER', 'user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'password'),
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
